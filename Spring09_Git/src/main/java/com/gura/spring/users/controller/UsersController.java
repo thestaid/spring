4e97f372b6dfa8a2615e6c23729aaced828af2e4
thestaid <thestaid@naver.com>
2017-01-20 11:27:07 +0900

    회원 가입 기능 완료

--- Spring09_Git/src/main/java/com/gura/spring/users/controller/UsersController.java
+++ Spring09_Git/src/main/java/com/gura/spring/users/controller/UsersController.java
@@ -2,12 +2,18 @@ package com.gura.spring.users.controller;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.gura.spring.users.dto.UsersDto;
 import com.gura.spring.users.service.UsersService;
 
 @Controller
@@ -15,6 +21,18 @@ public class UsersController {
 
 	@Autowired
 	private UsersService usersService;
+	
+	// "/users/signup.do" 요청처리
+	@RequestMapping("/users/signup")
+	public ModelAndView signup(HttpServletRequest request, @ModelAttribute UsersDto dto){
+		//서비스를 이용해서 회원 정보를 저장한다.
+		usersService.insert(dto);
+		ModelAndView mView=new ModelAndView();
+		mView.addObject("msg", dto.getId()+"회원님 가입되었습니다.");
+		mView.addObject("redirectUri", request.getContextPath());
+		mView.setViewName("users/alert");
+		return mView;
+	}
 	
 	// ajax "/users/checkid.do 요청 처리
 	@RequestMapping("/users/checkid")
