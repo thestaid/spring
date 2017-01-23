060a9085ee234d77d265af880479f0307ee2577c
thestaid <thestaid@naver.com>
2017-01-23 10:55:04 +0900

    saving new cafe text complete

--- Spring09_Git/src/main/java/com/gura/spring/cafe/controller/CafeController.java
+++ Spring09_Git/src/main/java/com/gura/spring/cafe/controller/CafeController.java
@@ -2,9 +2,11 @@ package com.gura.spring.cafe.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.gura.spring.cafe.dto.CafeDto;
 import com.gura.spring.cafe.service.CafeService;
 
 @Controller
@@ -26,5 +28,12 @@ public class CafeController {
 	@RequestMapping("/cafe/private/insertform")
 	public String insertForm(){
 		return "cafe/private/insertform";
+	}
+	
+	@RequestMapping("/cafe/private/insert")
+	public String insert(@ModelAttribute CafeDto dto){
+		cafeService.insert(dto);
+		return "redirect:/cafe/list.do";
+		
 	}
 }
